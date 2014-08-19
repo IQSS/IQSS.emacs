8270b3034a5099041a3069a0ee18b8ab31331ce0
Ista Zahn <istazahn@gmail.com>
2014-08-19 15:32:33 -0400

    remove variable pitch font override

--- custom.el
+++ custom.el
@@ -1,21 +1,3 @@
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(column-number-mode t)
- '(cua-mode t nil (cua-base))
- '(lpr-command "gtklp")
- '(show-paren-mode t))
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(variable-pitch ((t (:family "Liberation Sans")))))
-
-
 ;;; other overrides go here
 ;;; set up unicode
 (prefer-coding-system       'utf-8)
