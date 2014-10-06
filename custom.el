6742b33fc97cbb1dc3447a04f980bab04f48b24d
Ista Zahn <istazahn@gmail.com>
2014-10-06 10:11:05 -0400

    update buffer-coding-system variable

--- custom.el
+++ custom.el
@@ -4,7 +4,7 @@
 (set-default-coding-systems 'utf-8)
 (set-terminal-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
-(setq default-buffer-file-coding-system 'utf-8)                      
+(setq buffer-file-coding-system 'utf-8)                      
 (setq x-select-request-type '(UTF8_STRING COMPOUND_TEXT TEXT STRING))
 
 ;; start the server if not already started
