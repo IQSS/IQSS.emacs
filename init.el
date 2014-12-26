12cb945dba03b2a3ccddb4f35b6e294dffa9b195
Ista Zahn <istazahn@gmail.com>
2014-12-26 14:14:40 -0500

    make sure package list is refreshed before installing

--- init.el
+++ init.el
@@ -69,14 +69,12 @@
 ;; Activate package autoloads
 (package-initialize)
 
-;; Fetch the list of packages available
-(when (not package-archive-contents)
-  (package-refresh-contents))
-
 ;; Install packages in package-list if they are not already installed
-(dolist (package my-package-list)
-  (when (not (package-installed-p package))
-    (package-install package)))
+(unless (every #'package-installed-p my-package-list)
+  (package-refresh-contents)
+  (dolist (package my-package-list)
+    (when (not (package-installed-p package))
+      (package-install package))))
 
 ;; finally a theme I can live with!
 (load-theme 'leuven t) 
