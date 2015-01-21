234577f6dc847b216ecc138f41e16b25c1540f08
Ista Zahn <istazahn@gmail.com>
2015-01-21 10:21:35 -0500

    add function for opening in external apps from Dired

--- init.el
+++ init.el
@@ -545,6 +545,43 @@
           '(lambda()
              (setq truncate-lines 1)))
 
+;; open files in external programs
+;; (from http://ergoemacs.org/emacs/emacs_dired_open_file_in_ext_apps.html
+(defun xah-open-in-external-app (&optional file)
+  "Open the current file or dired marked files in external app.
+
+The app is chosen from your OS's preference."
+  (interactive)
+  (let (doIt
+        (myFileList
+         (cond
+          ((string-equal major-mode "dired-mode")
+           (dired-get-marked-files))
+          ((not file) (list (buffer-file-name)))
+          (file (list file)))))
+    (setq doIt (if (<= (length myFileList) 5)
+                   t
+                 (y-or-n-p "Open more than 5 files? "))) 
+    (when doIt
+      (cond
+       ((string-equal system-type "windows-nt")
+        (mapc
+         (lambda (fPath)
+           (w32-shell-execute "open" (replace-regexp-in-string "/" "\\" fPath t t)))
+         myFileList))
+       ((string-equal system-type "darwin")
+        (mapc
+         (lambda (fPath)
+           (shell-command (format "open \"%s\"" fPath)))
+         myFileList))
+       ((string-equal system-type "gnu/linux")
+        (mapc
+         (lambda (fPath)
+           (let ((process-connection-type nil))
+             (start-process "" nil "xdg-open" fPath))) myFileList))))))
+;; open files from dired with "E"
+(define-key dired-mode-map (kbd "E") 'xah-open-in-external-app)
+
 ;; shell
 (require 'essh) ; if not done elsewhere; essh is in the local lisp folder
 (require 'eval-in-repl-shell)
