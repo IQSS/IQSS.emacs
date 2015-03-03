41e1ecbf210ca9716b636fa28d6b5df0c5a3b241
Ista Zahn <istazahn@gmail.com>
2015-03-03 13:11:24 -0500

    mode-line indicator when working on remote file

--- init.el
+++ init.el
@@ -149,9 +149,83 @@
 ;; finally a theme I can live with!
 (load-theme 'leuven t) 
 (setq org-fontify-whole-heading-line t)
+
+;; mode line theme
 (require 'powerline)
-(powerline-default-theme)
-(powerline-default-theme)
+;; highlight hostname if on remote
+(defconst my-mode-line-buffer-identification
+  '(:eval
+    (list
+     (propertize
+      (if (file-remote-p default-directory 'host)
+          (progn
+      (let ((host-name
+             (or (file-remote-p default-directory 'host)
+                 (system-name))))
+        (if (string-match "^[^0-9][^.]*\\(\\..*\\)" host-name)
+            (substring host-name 0 (match-beginning 1))
+          host-name)))
+        "")
+      'face
+      (if (file-remote-p default-directory 'host)
+          'mode-line-highlight
+        'mode-line-buffer-id))
+   (propertize ": %b"
+               'face
+                 (if (file-remote-p default-directory 'host)
+                     'mode-line-highlight
+                   'mode-line-buffer-id)))))
+
+;; powerline theme using above info about remote hosts.
+(defun powerline-my-theme ()
+  "Setup the default mode-line."
+  (interactive)
+  (setq-default mode-line-format
+                '("%e"
+                  (:eval
+                   (let* ((active (powerline-selected-window-active))
+                          (mode-line (if active 'mode-line 'mode-line-inactive))
+                          (face1 (if active 'powerline-active1 'powerline-inactive1))
+                          (face2 (if active 'powerline-active2 'powerline-inactive2))
+                          (separator-left (intern (format "powerline-%s-%s"
+                                                          powerline-default-separator
+                                                          (car powerline-default-separator-dir))))
+                          (separator-right (intern (format "powerline-%s-%s"
+                                                           powerline-default-separator
+                                                           (cdr powerline-default-separator-dir))))
+                          (lhs (list (powerline-raw "%*" nil 'l)
+                                     (powerline-buffer-size nil 'l)
+                                     (powerline-raw mode-line-mule-info nil 'l)
+                                     (powerline-raw mode-line-remote nil 'l)
+                                     (powerline-raw my-mode-line-buffer-identification nil 'l)
+                                     (when (and (boundp 'which-func-mode) which-func-mode)
+                                       (powerline-raw which-func-format nil 'l))
+                                     (powerline-raw " ")
+                                     (funcall separator-left mode-line face1)
+                                     (when (boundp 'erc-modified-channels-object)
+                                       (powerline-raw erc-modified-channels-object face1 'l))
+                                     (powerline-major-mode face1 'l)
+                                     (powerline-process face1)
+                                     (powerline-minor-modes face1 'l)
+                                     (powerline-narrow face1 'l)
+                                     (powerline-raw " " face1)
+                                     (funcall separator-left face1 face2)
+                                     (powerline-vc face2 'r)))
+                          (rhs (list (powerline-raw global-mode-string face2 'r)
+                                     (funcall separator-right face2 face1)
+                                     (powerline-raw "%4l" face1 'l)
+                                     (powerline-raw ":" face1 'l)
+                                     (powerline-raw "%3c" face1 'r)
+                                     (funcall separator-right face1 mode-line)
+                                     (powerline-raw " ")
+                                     (powerline-raw "%6p" nil 'r)
+                                     (powerline-hud face2 face1))))
+                     (concat (powerline-render lhs)
+                             (powerline-fill face2 (powerline-width rhs))
+                             (powerline-render rhs)))))))
+
+(powerline-my-theme)
+(powerline-my-theme)
 
 ;; add custom lisp directory to path
 (let ((default-directory (concat user-emacs-directory "lisp/")))
