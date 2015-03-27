bed2d76f8a465ce8dc92646e8d3a5ac9e7cc0e2b
Ista Zahn <istazahn@gmail.com>
2015-03-27 13:09:27 -0400

    use custom code for frame restore, no desktop.el

--- init.el
+++ init.el
@@ -18,12 +18,59 @@
           24.2)
   (error "Your version of emacs is very old and must be upgraded before you can use these packages"))
 
-;; restore frames
-(setq desktop-load-locked-desktop nil)
-(setq desktop-buffers-not-to-save "^.*$")
-(setq desktop-files-not-to-save "^.*$")
-(setq desktop-save t)
-(desktop-save-mode 1)
+;; restore frames (from http://ck.kennt-wayne.de/2011/jul/emacs-restore-last-frame-size-on-startup)
+(defun save-framegeometry ()
+  "Gets the current frame's geometry and saves to ~/.emacs.d/framegeometry."
+  (let (
+        (framegeometry-left (frame-parameter (selected-frame) 'left))
+        (framegeometry-top (frame-parameter (selected-frame) 'top))
+        (framegeometry-width (frame-parameter (selected-frame) 'width))
+        (framegeometry-height (frame-parameter (selected-frame) 'height))
+        (framegeometry-file (expand-file-name "~/.emacs.d/framegeometry"))
+        )
+    
+    (when (not (number-or-marker-p framegeometry-left))
+      (setq framegeometry-left 0))
+    (when (not (number-or-marker-p framegeometry-top))
+      (setq framegeometry-top 0))
+    (when (not (number-or-marker-p framegeometry-width))
+      (setq framegeometry-width 0))
+    (when (not (number-or-marker-p framegeometry-height))
+      (setq framegeometry-height 0))
+    
+    (with-temp-buffer
+      (insert
+       ";;; This is the previous emacs frame's geometry.\n"
+       ";;; Last generated " (current-time-string) ".\n"
+       "(setq initial-frame-alist\n"
+       "      '(\n"
+       (format "        (top . %d)\n" (max framegeometry-top 0))
+       (format "        (left . %d)\n" (max framegeometry-left 0))
+       (format "        (width . %d)\n" (max framegeometry-width 0))
+       (format "        (height . %d)))\n" (max framegeometry-height 0)))
+      (when (file-writable-p framegeometry-file)
+        (write-file framegeometry-file))))
+  )
+
+(defun load-framegeometry ()
+  "Loads ~/.emacs.d/framegeometry which should load the previous frame's geometry."
+  (let ((framegeometry-file (expand-file-name "~/.emacs.d/framegeometry")))
+    (when (file-readable-p framegeometry-file)
+      (load-file framegeometry-file)))
+  )
+
+;; Special work to do ONLY when there is a window system being used
+(if window-system
+    (progn
+      (add-hook 'after-init-hook 'load-framegeometry)
+      (add-hook 'kill-emacs-hook 'save-framegeometry))
+  )
+
+;; (setq desktop-load-locked-desktop nil)
+;; (setq desktop-buffers-not-to-save "^.*$")
+;; (setq desktop-files-not-to-save "^.*$")
+;; (setq desktop-save t)
+;; (desktop-save-mode 1)
 
 ;; hide the toolbar
 (tool-bar-mode 0)
@@ -31,11 +78,11 @@
 ;; always use fancy-startup, even on small screens
 (defun always-use-fancy-splash-screens-p () 1)
 (defalias 'use-fancy-splash-screens-p 'always-use-fancy-splash-screens-p)
-(add-hook 'after-init-hook
-          (lambda()
-            (fancy-startup-screen t)
-            (switch-to-buffer "*GNU Emacs*")
-            (delete-other-windows)))
+;; (add-hook 'after-init-hook
+;;           (lambda()
+;;             (fancy-startup-screen t)
+;;             (switch-to-buffer "*GNU Emacs*")
+;;             (delete-other-windows)))
 
 ;;; Install required packages
 (require 'cl)
