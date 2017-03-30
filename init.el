7c6634181feeeea2baa58ce2ea9af4ee3f834508
Ista Zahn <istazahn@gmail.com>
2017-03-30 16:14:58 -0400

    fix text-zoom keybindings

--- init.el
+++ init.el
@@ -207,8 +207,8 @@
 (mode-icons-mode)
 
 ;; zoom in/out like we do everywhere else.
-(global-set-key (kbd "C-+" 'text-scale-increase))
-(global-set-key (kbd "C--" 'text-scale-decrease))
+(global-set-key (kbd "C-+") 'text-scale-increase)
+(global-set-key (kbd "C--") 'text-scale-decrease)
 
 ;; Work spaces
 (setq eyebrowse-keymap-prefix (kbd "C-c C-l"))
