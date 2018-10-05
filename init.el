971ea57652babb56a30932a7f95818c4596ae58b
Ista Zahn <istazahn@gmail.com>
2018-10-05 16:13:23 -0400

    use ipython if available

--- init.el
+++ init.el
@@ -80,6 +80,7 @@
         company-math
         web-mode
         markdown-mode
+        pandoc-mode
         polymode
         poly-markdown
         poly-org
@@ -797,13 +798,16 @@
   (define-key python-mode-map (kbd "C-c C-c") 'eir-eval-in-python)
   (define-key python-mode-map (kbd "<C-return>") 'eir-eval-in-python)
   (define-key python-mode-map (kbd "C-c C-b") 'python-shell-send-buffer)
-  (define-key python-mode-map (kbd "<C-S-return>") 'python-shell-send-buffer))
-
-;; make outline work
-(add-hook 'python-mode-hook
+  (define-key python-mode-map (kbd "<C-S-return>") 'python-shell-send-buffer)
+  ;; use ipython if we can
+  (when (executable-find "ipython")
+    (setq python-shell-interpreter "ipython"
+          python-shell-interpreter-args "--simple-prompt -i"))
+  ;; make outline work
+  (add-hook 'python-mode-hook
           (lambda()
             ;;(setq-local outline-regexp "[#]+")
-            (outline-minor-mode t)))
+            (outline-minor-mode t))))
 
 (when (executable-find "julia")
   (require 'julia-mode)
@@ -1270,6 +1274,10 @@ Will prompt you shell name when you type `C-u' before this command."
   (setq untitled-new-buffer-major-modes '(text-mode python-mode r-mode markdown-mode LaTeX-mode emacs-lisp-mode))
   ;; Change default buffer name.
   (setq untitled-new-buffer-default-name "*Untitled*")
+
+  ;; make sure emacs doesn't mess with our package list.
+  (defun
+      package--save-selected-packages (&rest opt) nil)
 
   ;; Start the server if it is not already running
   (require 'server)
