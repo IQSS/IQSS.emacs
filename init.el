fe318e19864a5237b1e838e30215c2e0bcda3974
Ista Zahn <istazahn@gmail.com>
2018-12-13 13:25:51 -0500

    avoid messing with emacs undo/redo system

--- init.el
+++ init.el
@@ -306,15 +306,34 @@
 (require 'win-win)
 
 ;; ;; Make control-z undo
+(let ((map (make-sparse-keymap)))
+  ;; remap `undo' and `undo-only' to `undo-tree-undo'
+  ;; (define-key map [remap undo] 'undo-tree-undo)
+  ;; (define-key map [remap undo-only] 'undo-tree-undo)
+  ;; bind standard undo bindings (since these match redo counterparts)
+  ;; (define-key map (kbd "C-/") 'undo-tree-undo)
+  ;; (define-key map "\C-_" 'undo-tree-undo)
+  ;; redo doesn't exist normally, so define our own keybindings
+  (define-key map (kbd "C-?") 'undo-tree-redo)
+  (define-key map (kbd "M-_") 'undo-tree-redo)
+  (define-key map (kbd "C-S-z") 'undo-tree-redo)
+  ;; just in case something has defined `redo'...
+  (define-key map [remap redo] 'undo-tree-redo)
+  ;; we use "C-x U" for the undo-tree visualizer
+  (define-key map (kbd "\C-x U") 'undo-tree-visualize)
+  ;; bind register commands
+  (define-key map (kbd "C-x r u") 'undo-tree-save-state-to-register)
+  (define-key map (kbd "C-x r U") 'undo-tree-restore-state-from-register)
+  ;; set keymap
+  (setq undo-tree-map map))
+
 (global-undo-tree-mode t)
-(global-set-key (kbd "C-z") 'undo)
-(define-key undo-tree-map (kbd "C-S-z") 'undo-tree-redo)
-(define-key undo-tree-map (kbd "C-x u") 'undo)
-(define-key undo-tree-map (kbd "C-x U") 'undo-tree-visualize)
-(define-key undo-tree-map (kbd "M-z") 'undo-tree-visualize)
+
 ;; Make C-g quit undo tree
 (define-key undo-tree-visualizer-mode-map (kbd "C-g") 'undo-tree-visualizer-quit)
 (define-key undo-tree-visualizer-mode-map (kbd "<escape> <escape> <escape>") 'undo-tree-visualizer-quit)
+
+
 
 ;;
 ;; Make right-click do something close to what people expect
