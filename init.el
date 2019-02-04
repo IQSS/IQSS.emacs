36d8f682929c582ab7500ff5d75b2d38c03e03d5
Ista Zahn <istazahn@gmail.com>
2019-02-04 10:55:40 -0500

    more conservative tab completion

--- init.el
+++ init.el
@@ -51,7 +51,6 @@
         yasnippet
         yasnippet-snippets
         multiple-cursors
-        smart-tab
         eglot
         visual-regexp
         command-log-mode
@@ -582,42 +581,45 @@
       dumb-jump-default-project "./")
 
 (require 'company)
-(company-quickhelp-mode)
-;; cancel if input doesn't match, be patient, and don't complete automatically.
-(setq company-require-match nil
-      company-async-timeout 6
-      company-idle-delay 5
-      company-minimum-prefix-length 1
-      company-global-modes '(not term-mode))
-;; use C-n and C-p to cycle through completions
-(define-key company-active-map (kbd "C-n") 'company-select-next)
-(define-key company-active-map (kbd "<tab>") 'company-complete-common)
-(define-key company-active-map (kbd "C-p") 'company-select-previous)
-(define-key company-active-map (kbd "<backtab>") 'company-select-previous)
+  (company-quickhelp-mode)
+  ;; cancel if input doesn't match, be patient, and don't complete automatically.
+  (setq company-require-match nil
+        company-async-timeout 6
+        company-idle-delay 5
+        company-minimum-prefix-length 1
+        company-global-modes '(not term-mode))
+  ;; use C-n and C-p to cycle through completions
+  (define-key company-active-map (kbd "C-n") 'company-select-next)
+  (define-key company-active-map (kbd "<tab>") 'company-complete-common)
+  (define-key company-active-map (kbd "C-p") 'company-select-previous)
+  (define-key company-active-map (kbd "<backtab>") 'company-select-previous)
 
-(require 'company-capf)
-(require 'company-files)
-(require 'company-math)
-(setq company-backends '((company-files company-math-symbols-unicode company-capf)))
-(setq-default company-backends '((company-files company-math-symbols-unicode company-capf)))
+  (require 'company-capf)
+  (require 'company-files)
+  (require 'company-math)
+  (setq company-backends '((company-files company-math-symbols-unicode company-capf)))
+  (setq-default company-backends '((company-files company-math-symbols-unicode company-capf)))
+  (setq tab-always-indent 'complete)
 
-;; completion key bindings
-(define-key company-mode-map (kbd "C-M-i") 'company-complete)
-(define-key company-mode-map (kbd "C-M-S-i") 'counsel-company)
-(require 'smart-tab)
-(require 'eglot)
-(setq eglot-ignored-server-capabilites
-      '(:documentHighlightProvider :hoverProvider))
-(setq smart-tab-expand-eolp t
-      smart-tab-user-provided-completion-function 'company-complete)
-;; (add-hook 'prog-mode-hook 'smart-tab-mode-on)
-(global-smart-tab-mode)
+  ;; completion key bindings
+  (define-key company-mode-map (kbd "C-M-i") 'company-complete)
+  (define-key company-mode-map (kbd "C-M-S-i") 'counsel-company)
+  (defun complete-or-indent ()
+    (interactive)
+    (if (and (looking-at "\\_>") (company-manual-begin))
+        (company-complete-common)
+      (indent-for-tab-command)))
+  (define-key company-mode-map (kbd "<tab>") 'complete-or-indent)
 
- ;; make company use pcomplete (via capf)
- ;; (add-hook 'completion-at-point-functions 'pcomplete-completions-at-point)
+;; make company use pcomplete (via capf)
+   ;; (add-hook 'completion-at-point-functions 'pcomplete-completions-at-point)
 
- ;; not sure why this should be set in a hook, but that is how the manual says to do it.
- (add-hook 'after-init-hook 'global-company-mode)
+   ;; not sure why this should be set in a hook, but that is how the manual says to do it.
+  (add-hook 'after-init-hook 'global-company-mode)
+
+  (require 'eglot)
+  (setq eglot-ignored-server-capabilites
+        '(:documentHighlightProvider :hoverProvider))
 
 ;; which-key settings taken mostly from https://github.com/aculich/.emacs.d/blob/master/init.el
 (with-eval-after-load "which-key"
