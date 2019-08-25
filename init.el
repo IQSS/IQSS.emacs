26eef821ffa6245d99c3aa7812057662f74d4f14
Ista Zahn <istazahn@gmail.com>
2019-08-25 08:33:04 -0400

    explicitly load ein

--- init.el
+++ init.el
@@ -733,6 +733,11 @@
 (setq comint-scroll-to-bottom-on-output t)
 (setq comint-move-point-for-output t)
 
+(when (executable-find "jupyter")
+  (require 'ein)
+  (require 'ein-notebook)
+  (require 'ein-subpackages))
+
 ;;;  ESS (Emacs Speaks Statistics)
 (with-eval-after-load "ess"
   (add-hook 'ess-r-mode-hook 'eglot-ensure)
