0b677c40f5ccd7559ad554acd5d835b52f3c77b8
Ista Zahn <izahn@hbs.edu>
2020-07-02 21:06:27 -0400

    Auto-suggest completions sooner.

--- init.el
+++ init.el
@@ -603,9 +603,9 @@
 (require 'company)
 ;; cancel if input doesn't match, be patient, and don't complete automatically.
 (setq company-require-match nil
-      company-async-timeout 6
-      company-idle-delay 5
-      company-minimum-prefix-length 0
+      company-async-timeout 2
+      company-idle-delay 1
+      company-minimum-prefix-length 2
       company-global-modes '(not term-mode))
 ;; use C-n and C-p to cycle through completions
 (define-key company-active-map (kbd "C-n") 'company-select-next)
@@ -626,10 +626,6 @@
 (define-key company-mode-map (kbd "C-M-i") 'company-complete)
 (define-key company-mode-map (kbd "C-M-S-i") 'counsel-company)
 
-;; make company use pcomplete (via capf)
-;; (add-hook 'completion-at-point-functions 'pcomplete-completions-at-point)
-
-;; not sure why this should be set in a hook, but that is how the manual says to do it.
 (add-hook 'after-init-hook 'global-company-mode)
 
 ;; which-key settings taken mostly from https://github.com/aculich/.emacs.d/blob/master/init.el
